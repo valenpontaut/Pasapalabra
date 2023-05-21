d8acf6a966d176be85a55ceb092fbe882695fa2a
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-05-21 12:56:19 -0300

    hotfix etapa4

--- etapa4.py
+++ etapa4.py
@@ -1,5 +1,5 @@
 from etapa1_etapa5 import iniciar_juego 
-from etapa2 import crear_diccionario, mostrar_total_de_palabras, total_de_palabras
+from etapa2 import crear_diccionario, total_de_palabras
 from etapa3 import crear_lista_letras, seleccionar_palabra
 
 def main():
@@ -13,12 +13,14 @@ def main():
     """
     print("¡Bienvenido al juego Pasapalabra!\n\nA continuación le mostraremos una serie de letras participantes de las cuales deberá intentar adivinar a qué palabra se está refiriendo leyendo su definición.\nDebajo de las letras podrá observar cuáles va acertando \"a\" o cuales va errando \"e\".\n\n¡Mucha suerte!")
     letras = ['a', 'b', 'c', 'd', 'e', 'f' ,'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    # 1. Generación del diccionario de palabras
     diccionario_palabras = crear_diccionario()
     cant_por_letra = total_de_palabras(diccionario_palabras)
-    # sacar prints de etapa 1, 2 y 3 // acentos en etapa 3 // dicc cant de palabras por letra y pasarrlo a lista //
-    cant_por_letra = sorted(cant_por_letra.items(), key = lambda x:x[0])
+    # 2. Selección de las 10 letras participantes
     letras_participantes = crear_lista_letras(letras)
+    # 3.  Selección al azar de la lista de palabras a adivinar por el jugador
     definiciones = seleccionar_palabra(diccionario_palabras, letras_participantes, cant_por_letra)
+    # 4. Armado del tablero y comienzo de la partida
     confirmacion = iniciar_juego(letras_participantes,definiciones)
     while confirmacion == "si":
         letras_participantes = crear_lista_letras(letras)
